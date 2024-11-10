cf86f52d545f59cd605bfc0f5c270556399802c0
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-11-10 16:45:46 +0900

    Title: 특정 조건 세 정수 비교, Time: 43ms, Memory: 15MB, Status: Passed - CodeTree

--- 241110/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 241110/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -4,9 +4,9 @@ a = int(a)
 b = int(b)
 c = int(c)
 
-if ( a < b and a < c ):
+if ( a <= b and a <= c ):
     minNumber = a
-elif ( b < a and b < c ):
+elif ( b <= a and b <= c ):
     minNumber = b
 else :
     minNumber = c
