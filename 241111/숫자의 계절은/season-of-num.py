5cc6c246a7ce2a6a33efda3c0fe117e6e192176b
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-11-11 14:55:08 +0900

    Title: 숫자의 계절은, Time: 42ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241111/숫자의 계절은/season-of-num.py
@@ -0,0 +1,10 @@
+month = int(input())
+
+if ( month == 12 or month == 1 or month == 2):
+    print("Winter")
+elif ( month == 3 or month == 4 or month == 5):
+    print("Spring")
+elif ( month == 6 or month == 7 or month == 8):
+    print("Summer")
+else :
+    print("Fall")
