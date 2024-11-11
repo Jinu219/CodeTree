6a85bb9e345556fc5ef140bc730ceaf83717ab67
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-11-11 15:40:15 +0900

    Title: 코로나 메뉴얼, Time: 73ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241111/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,15 @@
+firstperson = input()
+secondperson = input()
+thirdperson = input()
+
+if(firstperson[0] == 'Y' and int(firstperson[1]) >= 37):
+    result += 1
+if(secondperson[0] == 'Y' and int(secondperson[1]) >= 37):
+    result += 1
+if(thirdperson[0] == 'Y' and int(thirdperson[1]) >= 37):
+    result += 1
+
+if(result >= 2 ):
+    print("E")
+else :
+    print("N")
